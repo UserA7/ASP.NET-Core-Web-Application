a5eb7aff7b6b40379a1f6d1c8d8f7427e89f79b2
Annie17 <raykova.adriana@abv.bg>
2023-06-06 23:21:17 +0300

    Adding Asp.Net Core Identity (Login and Registration)

--- PlaylistMVC/Program.cs
+++ PlaylistMVC/Program.cs
@@ -1,13 +1,30 @@
+using FluentAssertions.Common;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using PlaylistMVC.Models;
+using Microsoft.AspNetCore.Identity;
+using PlaylistMVC.Data;
+using PlaylistMVC.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //Adding database context
+
 builder.Services.AddDbContext<MyDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDbContext<AuthDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AuthDbContextConnection")));
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<AuthDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireUppercase = false;
+});
 
 var app = builder.Build();
 
@@ -24,10 +41,19 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+
+//using authentication
+app.UseAuthentication();
+
 app.UseAuthorization();
+
+
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapRazorPages();
+
 app.Run();
+
